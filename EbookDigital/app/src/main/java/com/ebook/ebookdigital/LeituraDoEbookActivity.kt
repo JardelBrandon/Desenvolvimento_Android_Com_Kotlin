6df871f9efad785ac18205070f56ef14e3acfcbf
Jardel Brandon de Araujo Regis <jardelbrandon@gmail.com>
2021-01-17 15:00:56 -0300

    App - Ebook Digital
    Carregando PDF no Aplicativo

--- EbookDigital/app/src/main/java/com/ebook/ebookdigital/LeituraDoEbookActivity.kt
+++ EbookDigital/app/src/main/java/com/ebook/ebookdigital/LeituraDoEbookActivity.kt
@@ -2,11 +2,21 @@ package com.ebook.ebookdigital
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.barteksc.pdfviewer.scroll.DefaultScrollHandle
+import kotlinx.android.synthetic.main.activity_leitura_do_ebook.*
 
 class LeituraDoEbookActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_leitura_do_ebook)
+
+        pdfView.fromAsset("ebook.pdf")
+            .defaultPage(0)
+            .enableDoubletap(true)
+            .enableSwipe(true)
+            .scrollHandle(DefaultScrollHandle(this))
+            .swipeHorizontal(false)
+            .load()
     }
 }
