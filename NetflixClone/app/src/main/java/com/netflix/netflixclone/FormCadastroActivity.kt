c37c5b316582fd7454df877d1774f203e02fa2cf
Jardel Brandon de Araujo Regis <jardelbrandon@gmail.com>
2021-01-17 20:42:46 -0300

    App - Netflix Clone
    Tratando Excetions

--- NetflixClone/app/src/main/java/com/netflix/netflixclone/FormCadastroActivity.kt
+++ NetflixClone/app/src/main/java/com/netflix/netflixclone/FormCadastroActivity.kt
@@ -3,7 +3,11 @@ package com.netflix.netflixclone
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.firebase.FirebaseNetworkException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException
 import kotlinx.android.synthetic.main.activity_form_cadastro.*
 
 class FormCadastroActivity : AppCompatActivity() {
@@ -32,6 +36,17 @@ class FormCadastroActivity : AppCompatActivity() {
 
                 if(it.isSuccessful) {
                     Toast.makeText(this, "Cadastro realizado com sucesso!", Toast.LENGTH_SHORT).show()
+                }
+            }.addOnFailureListener {
+
+                var erro = it
+
+                when {
+                    erro is FirebaseAuthWeakPasswordException -> mensagens.setText("Digite uma senha com no mínimo 6 caracteres!")
+                    erro is FirebaseAuthInvalidCredentialsException -> mensagens.setText("Digite um e-mail válido!")
+                    erro is FirebaseAuthUserCollisionException -> mensagens.setText("Esta conta já foi cadastrada!")
+                    erro is FirebaseNetworkException -> mensagens.setText("Sem conexão com a internet!")
+                    else -> mensagens.setText("Erro ao cadastrar usuário!")
                 }
             }
         }
