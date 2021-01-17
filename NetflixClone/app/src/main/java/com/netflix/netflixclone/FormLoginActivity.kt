004945a20b99f3e32320c09b0de69fa2751f411d
Jardel Brandon de Araujo Regis <jardelbrandon@gmail.com>
2021-01-17 17:34:20 -0300

    App - Netflix Clone
    Construindo a tela de Cadastro

--- NetflixClone/app/src/main/java/com/netflix/netflixclone/FormLoginActivity.kt
+++ NetflixClone/app/src/main/java/com/netflix/netflixclone/FormLoginActivity.kt
@@ -1,12 +1,26 @@
 package com.netflix.netflixclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_form_login.*
 
 class FormLoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form_login)
+
+        var textoCadastrar = text_cadastrar
+
+        textoCadastrar.setOnClickListener {
+            abrirTelaDeCadastro()
+        }
     }
+
+    private fun abrirTelaDeCadastro() {
+        var intent = Intent(this, FormCadastroActivity::class.java)
+        startActivity(intent)
+    }
+
 }
