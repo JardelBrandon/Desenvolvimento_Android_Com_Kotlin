da28a450d009a98d624e7de824b119653e3ef1fc
Jardel Brandon de Araujo Regis <jardelbrandon@gmail.com>
2021-01-17 13:47:46 -0300

    App - Cardápio de Restaurante
    Criando uma Toolbar

--- CardapioDeRestaurante/app/src/main/java/com/cardapio/cardapioderestaurante/SucosActivity.kt
+++ CardapioDeRestaurante/app/src/main/java/com/cardapio/cardapioderestaurante/SucosActivity.kt
@@ -1,12 +1,24 @@
 package com.cardapio.cardapioderestaurante
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.toolbar.*
 
 class SucosActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sucos)
+        supportActionBar!!.hide()
+
+        toolbar.title = "Sucos"
+        toolbar.setNavigationIcon(getDrawable(R.drawable.ic_voltar))
+        toolbar.setNavigationOnClickListener {
+
+            var intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
