8dfcfd75473935f3fc0284abd5959fe3afaa7328
Jardel Brandon de Araujo Regis <jardelbrandon@gmail.com>
2021-01-17 19:40:59 -0300

    App - Netflix Clone
    Cadastrando usuários no Firebase

--- NetflixClone/app/src/main/java/com/netflix/netflixclone/FormCadastroActivity.kt
+++ NetflixClone/app/src/main/java/com/netflix/netflixclone/FormCadastroActivity.kt
@@ -2,11 +2,38 @@ package com.netflix.netflixclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_form_cadastro.*
 
 class FormCadastroActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form_cadastro)
+
+        bt_cadastrar.setOnClickListener {
+
+            cadastrarUsuario()
+        }
+    }
+
+    private fun cadastrarUsuario() {
+
+        var email = edit_email.text.toString()
+        var senha = edit_senha.text.toString()
+        var mensagens = mensagem_erro
+
+        if(email.isEmpty() || senha.isEmpty()) {
+            mensagens.setText("Coloque o seu e-mail e sua senha!")
+        } else {
+
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, senha).addOnCompleteListener {
+
+                if(it.isSuccessful) {
+                    Toast.makeText(this, "Cadastro realizado com sucesso!", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 }
