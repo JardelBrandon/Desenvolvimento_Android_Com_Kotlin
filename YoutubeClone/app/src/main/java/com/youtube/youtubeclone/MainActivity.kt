6f076bfbd995e2cf8f0440676b4c9d966a75bd44
Jardel Brandon de Araujo Regis <jardelbrandon@gmail.com>
2021-01-18 15:06:37 -0300

    App - Youtube Clone
    Criando um Menu

--- YoutubeClone/app/src/main/java/com/youtube/youtubeclone/MainActivity.kt
+++ YoutubeClone/app/src/main/java/com/youtube/youtubeclone/MainActivity.kt
@@ -2,11 +2,27 @@ package com.youtube.youtubeclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.youtube.youtubeclone.Adapter.VideosAdapter
+import com.youtube.youtubeclone.Model.addVideos
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recycler_videos.adapter = VideosAdapter(addVideos())
+        recycler_videos.layoutManager = LinearLayoutManager(this)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        val inflate = menuInflater
+        inflate.inflate((R.menu.menu_youtube), menu)
+
+        return true
     }
 }
