4c2e061d7b0220812d28c2bc641d9902c2b59a4a
Jardel Brandon de Araujo Regis <jardelbrandon@gmail.com>
2021-01-19 10:46:23 -0300

    App - Loja Virtual
    Criando slides com o Material-Intro

---
+++ LojaVirtual/app/src/main/java/com/loja/lojavirtual/SlidesActivity.kt
@@ -0,0 +1,36 @@
+package com.loja.lojavirtual
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.heinrichreimersoftware.materialintro.app.IntroActivity
+import com.heinrichreimersoftware.materialintro.slide.SimpleSlide
+
+class SlidesActivity : IntroActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_slides)
+
+        addSlide(
+
+            SimpleSlide.Builder()
+                .background(R.color.roxo)
+                .image(R.drawable.drawer)
+                .title("Loja Online de Calçados")
+                .description("Entre e confira as promoções que preparamos para você!")
+                .build()
+
+        )
+
+        addSlide(
+
+            SimpleSlide.Builder()
+                .background(R.color.azulEsverdiado)
+                .title("Crie uma conta Grátis")
+                .description("Cadastre-se agora mesmo! E venha conhecer os nossos produtos.")
+                .build()
+
+        )
+
+    }
+}
