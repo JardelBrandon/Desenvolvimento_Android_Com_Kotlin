88ec1fc2ccdb5e013df31c91bfc2208d78a64234
Jardel Brandon de Araujo Regis <jardelbrandon@gmail.com>
2021-01-19 14:07:24 -0300

    App - Loja Virtual
    Trabalhando com Fragmentos

--- LojaVirtual/app/src/main/java/com/loja/lojavirtual/TelaPrincipalActivity.kt
+++ LojaVirtual/app/src/main/java/com/loja/lojavirtual/TelaPrincipalActivity.kt
@@ -2,8 +2,10 @@ package com.loja.lojavirtual
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Gravity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
@@ -15,10 +17,13 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import com.google.firebase.auth.FirebaseAuth
 import com.loja.lojavirtual.Form.FormLoginActivity
+import com.loja.lojavirtual.Fragments.ProdutosFragment
+import kotlinx.android.synthetic.main.activity_form_login.*
 
-class TelaPrincipalActivity : AppCompatActivity() {
+class TelaPrincipalActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
 
@@ -28,18 +33,47 @@ class TelaPrincipalActivity : AppCompatActivity() {
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
+        val produtosFragment = ProdutosFragment()
+        val fragment = supportFragmentManager.beginTransaction()
+        fragment.replace(R.id.frame_container, produtosFragment)
+        fragment.commit()
+
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
-            ), drawerLayout
+        val toggle = ActionBarDrawerToggle(
+
+            this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        navView.setNavigationItemSelectedListener(this)
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        val id = item.itemId
+
+        if (id == R.id.nav_produtos) {
+
+            val produtosFragment = ProdutosFragment()
+            val fragment = supportFragmentManager.beginTransaction()
+            fragment.replace(R.id.frame_container, produtosFragment)
+            fragment.commit()
+
+
+        } else if (id == R.id.nav_cadastrar_produtos) {
+
+
+        } else if (id == R.id.nav_contato) {
+
+
+        }
+
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+        drawer.closeDrawer(GravityCompat.START)
+        return true
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -68,8 +102,4 @@ class TelaPrincipalActivity : AppCompatActivity() {
         finish()
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
-    }
 }
