f60cee4680d465a6f638c52cd33ec8a4082a903b
Jardel Brandon de Araujo Regis <jardelbrandon@gmail.com>
2021-01-21 20:35:56 -0300

    App - Spotify Clone
    Adicionando Fragmentos

---
+++ SpotifyClone/app/src/main/java/com/app/spotifyclone/Fragments/BuscarFragment.kt
@@ -0,0 +1,33 @@
+package com.app.spotifyclone.Fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.app.spotifyclone.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class BuscarFragment : Fragment() {
+
+    companion object {
+        fun newInstance(): BuscarFragment {
+            val fragmentBuscar = BuscarFragment()
+            val argumentos = Bundle()
+            fragmentBuscar.arguments = argumentos
+            return fragmentBuscar
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_buscar, container, false)
+    }
+
+}
--- SpotifyClone/app/src/main/java/com/app/spotifyclone/MainActivity.kt
+++ SpotifyClone/app/src/main/java/com/app/spotifyclone/MainActivity.kt
@@ -2,11 +2,52 @@ package com.app.spotifyclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.fragment.app.Fragment
+import com.app.spotifyclone.Fragments.BibliotecaFragment
+import com.app.spotifyclone.Fragments.BuscarFragment
+import com.app.spotifyclone.Fragments.HomeFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var content: FrameLayout? = null
+    private var mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener {item ->
+        when (item.itemId) {
+            R.id.nav_inicio -> {
+                val fragment = HomeFragment.newInstance()
+                addFragment(fragment)
+            }
+
+            R.id.nav_buscar -> {
+                val fragment = BuscarFragment.newInstance()
+                addFragment(fragment)
+            }
+
+            R.id.nav_biblioteca -> {
+                val fragment = BibliotecaFragment.newInstance()
+                addFragment(fragment)
+            }
+        }
+        false
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        content = content
+        bottom_menu.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+        val fragment = HomeFragment.newInstance()
+        addFragment(fragment)
+    }
+
+    private fun addFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.content, fragment, fragment.javaClass.simpleName)
+            .commit()
     }
 }
