adba3396ab74722d6bcc20c523c0552611a2c218
Jardel Brandon de Araujo Regis <jardelbrandon@gmail.com>
2021-01-20 21:18:26 -0300

    App - Loja Virtual
    Selecionando imagens da galeria de fotos

--- LojaVirtual/app/src/main/java/com/loja/lojavirtual/Fragments/CadastroProdutosActivity.kt
+++ LojaVirtual/app/src/main/java/com/loja/lojavirtual/Fragments/CadastroProdutosActivity.kt
@@ -1,13 +1,47 @@
 package com.loja.lojavirtual.Fragments
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import com.loja.lojavirtual.R
+import kotlinx.android.synthetic.main.activity_cadastro_produtos.*
 
 class CadastroProdutosActivity : AppCompatActivity() {
+
+    private var selecionarUri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro_produtos)
+
+        bt_selecionar_foto.setOnClickListener {
+
+            selecionarFotoDaGaleria()
+
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 0) {
+
+            selecionarUri = data?.data
+
+            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, selecionarUri)
+            foto_produto.setImageBitmap(bitmap)
+            bt_selecionar_foto.alpha = 0f
+
+        }
+    }
+
+    private fun selecionarFotoDaGaleria() {
+
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, 0)
+
     }
 }
