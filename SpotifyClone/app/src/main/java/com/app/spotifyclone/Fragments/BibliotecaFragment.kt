9bfefa4fd885e9b07664df0839b6415e49e1cf21
Jardel Brandon de Araujo Regis <jardelbrandon@gmail.com>
2021-01-23 11:48:32 -0300

    App - Spotify Clone
    SmartTabLayout

--- SpotifyClone/app/src/main/java/com/app/spotifyclone/Fragments/BibliotecaFragment.kt
+++ SpotifyClone/app/src/main/java/com/app/spotifyclone/Fragments/BibliotecaFragment.kt
@@ -5,8 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentPagerAdapter
+import com.app.spotifyclone.FragmentsTab.AlbunsFragment
+import com.app.spotifyclone.FragmentsTab.ArtistasFragment
+import com.app.spotifyclone.FragmentsTab.PlaylistsFragment
 
 import com.app.spotifyclone.R
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems
+import kotlinx.android.synthetic.main.fragment_biblioteca.*
 
 /**
  * A simple [Fragment] subclass.
@@ -28,6 +35,18 @@ class BibliotecaFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_biblioteca, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        var adapter = FragmentPagerItemAdapter(fragmentManager, FragmentPagerItems.with(context)
+            .add("Playlists", PlaylistsFragment::class.java)
+            .add("Artistas", ArtistasFragment::class.java)
+            .add("Álbuns", AlbunsFragment::class.java)
+            .create())
+
+        viewpager.adapter = adapter
+        viewpagertab.setViewPager(viewpager)
     }
 
 }
