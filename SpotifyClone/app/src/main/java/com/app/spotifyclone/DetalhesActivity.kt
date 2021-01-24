3c612f1f0a6930b9c85ded53f68b061b9144bb05
Jardel Brandon de Araujo Regis <jardelbrandon@gmail.com>
2021-01-24 00:51:04 -0300

    App - Spotify Clone
    Criando Toolbar, Trabalhando com efeitos, Mudando a cor da StatusBar

--- SpotifyClone/app/src/main/java/com/app/spotifyclone/DetalhesActivity.kt
+++ SpotifyClone/app/src/main/java/com/app/spotifyclone/DetalhesActivity.kt
@@ -1,12 +1,31 @@
 package com.app.spotifyclone
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.app.spotifyclone.Fragments.HomeFragment
+import kotlinx.android.synthetic.main.activity_detalhes.*
 
 class DetalhesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalhes)
+
+        window.statusBarColor = Color.LTGRAY
+        toolbar.setNavigationIcon(getDrawable(R.drawable.ic_arrow_back))
+        toolbar.title = null
+        toolbar.setNavigationOnClickListener {
+
+            var intent = Intent(this, HomeFragment::class.java)
+            startActivities(intent)
+            finish()
+
+        }
+    }
+
+    private fun startActivities(intent: Intent) {
+
     }
 }
